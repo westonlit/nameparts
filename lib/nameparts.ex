013da917586f642dc8852221b384898173d2f42f
Weston Littrell <westonlittrell@gmail.com>
2018-02-12 12:08:57 -0600

    add part lists as modules attributes; mix format

--- lib/nameparts.ex
+++ lib/nameparts.ex
@@ -2,7 +2,9 @@ defmodule Nameparts do
   @moduledoc """
   Documentation for Nameparts.
   """
+  alias __MODULE__
   @enforce_keys [:full_name]
+
   defstruct [
     :full_name,
     :salutation,
@@ -17,6 +19,143 @@ defmodule Nameparts do
     :has_ln_prefix,
     :has_supplemental_info
   ]
+
+  @salutations = [
+    "MR",
+    "MS",
+    "MRS",
+    "DR",
+    "MISS",
+    "DOCTOR",
+    "CORP",
+    "SGT",
+    "PVT",
+    "JUDGE",
+    "CAPT",
+    "COL",
+    "MAJ",
+    "LT",
+    "LIEUTENANT",
+    "PRM",
+    "PATROLMAN",
+    "HON",
+    "OFFICER",
+    "REV",
+    "PRES",
+    "PRESIDENT",
+    "GOV",
+    "GOVERNOR",
+    "VICE PRESIDENT",
+    "VP",
+    "MAYOR",
+    "SIR",
+    "MADAM",
+    "HONERABLE"
+  ]
+
+  @generations = [
+    "JR",
+    "SR",
+    "I",
+    "II",
+    "III",
+    "IV",
+    "V",
+    "VI",
+    "VII",
+    "VIII",
+    "IX",
+    "X",
+    "1ST",
+    "2ND",
+    "3RD",
+    "4TH",
+    "5TH",
+    "6TH",
+    "7TH",
+    "8TH",
+    "9TH",
+    "10TH",
+    "FIRST",
+    "SECOND",
+    "THIRD",
+    "FOURTH",
+    "FIFTH",
+    "SIXTH",
+    "SEVENTH",
+    "EIGHTH",
+    "NINTH",
+    "TENTH"
+  ]
+
+  @suffixes = ["ESQ", "PHD", "MD"]
+
+  @ln_prefixes = [
+    "DE",
+    "DA",
+    "DI",
+    "LA",
+    "DU",
+    "DEL",
+    "DEI",
+    "VDA",
+    "DELLO",
+    "DELLA",
+    "DEGLI",
+    "DELLE",
+    "VAN",
+    "VON",
+    "DER",
+    "DEN",
+    "HEER",
+    "TEN",
+    "TER",
+    "VANDE",
+    "VANDEN",
+    "VANDER",
+    "VOOR",
+    "VER",
+    "AAN",
+    "MC",
+    "BEN",
+    "SAN",
+    "SAINZ",
+    "BIN",
+    "LI",
+    "LE",
+    "DES",
+    "AM",
+    "AUS'M",
+    "VOM",
+    "ZUM",
+    "ZUR",
+    "TEN",
+    "IBN"
+  ]
+
+  @no_name = ["AKA", "FKA", "NKA", "FICTITIOUS"]
+
+  @corp_entity = [
+    "NA",
+    "CORP",
+    "CO",
+    "INC",
+    "ASSOCIATES",
+    "SERVICE",
+    "LLC",
+    "LLP",
+    "PARTNERS",
+    "RA",
+    "CO",
+    "COUNTY",
+    "STATE",
+    "BANK",
+    "GROUP",
+    "MUTUAL",
+    "FARGO"
+  ]
+
+  @supplemental_info = ["WIFE OF", "HUSBAND OF", "SON OF", "DAUGHTER OF", "DECEASED"]
 
   @doc """
   Hello world.
