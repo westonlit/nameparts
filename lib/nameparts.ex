6b5e9ab0bc86bbb3305d6efe79d64482da9c9ddd
Weston Littrell <westonlittrell@gmail.com>
2018-02-13 17:15:40 -0600

    port functionality and pass existing tests

--- lib/nameparts.ex
+++ lib/nameparts.ex
@@ -2,7 +2,6 @@ defmodule Nameparts do
   @moduledoc """
   Documentation for Nameparts.
   """
-  alias __MODULE__
   @enforce_keys [:full_name]
 
   defstruct [
@@ -14,159 +13,279 @@ defmodule Nameparts do
     :generation,
     :suffix,
     :aliases,
-    :has_corporate_entity,
-    :has_non_name,
-    :has_ln_prefix,
-    :has_supplemental_info
+    has_corporate_entity: false,
+    has_non_name: false,
+    has_ln_prefix: false,
+    has_supplemental_info: false
   ]
 
-  @salutations = [
-    "MR",
-    "MS",
-    "MRS",
-    "DR",
-    "MISS",
-    "DOCTOR",
-    "CORP",
-    "SGT",
-    "PVT",
-    "JUDGE",
-    "CAPT",
-    "COL",
-    "MAJ",
-    "LT",
-    "LIEUTENANT",
-    "PRM",
-    "PATROLMAN",
-    "HON",
-    "OFFICER",
-    "REV",
-    "PRES",
-    "PRESIDENT",
-    "GOV",
-    "GOVERNOR",
-    "VICE PRESIDENT",
-    "VP",
-    "MAYOR",
-    "SIR",
-    "MADAM",
-    "HONERABLE"
+  @salutations [
+    "MR", "MS", "MRS", "DR", "MISS", "DOCTOR", "CORP", "SGT", "PVT", "JUDGE",
+    "CAPT", "COL", "MAJ", "LT", "LIEUTENANT", "PRM", "PATROLMAN", "HON",
+    "OFFICER", "REV", "PRES", "PRESIDENT", "GOV", "GOVERNOR", "VICE PRESIDENT",
+    "VP", "MAYOR", "SIR", "MADAM", "HONERABLE"
   ]
 
-  @generations = [
-    "JR",
-    "SR",
-    "I",
-    "II",
-    "III",
-    "IV",
-    "V",
-    "VI",
-    "VII",
-    "VIII",
-    "IX",
-    "X",
-    "1ST",
-    "2ND",
-    "3RD",
-    "4TH",
-    "5TH",
-    "6TH",
-    "7TH",
-    "8TH",
-    "9TH",
-    "10TH",
-    "FIRST",
-    "SECOND",
-    "THIRD",
-    "FOURTH",
-    "FIFTH",
-    "SIXTH",
-    "SEVENTH",
-    "EIGHTH",
-    "NINTH",
-    "TENTH"
+  @generations [
+    "JR", "SR", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
+    "1ST", "2ND", "3RD", "4TH", "5TH", "6TH", "7TH", "8TH", "9TH", "10TH",
+    "FIRST", "SECOND", "THIRD", "FOURTH", "FIFTH", "SIXTH", "SEVENTH",
+    "EIGHTH", "NINTH", "TENTH"
   ]
 
-  @suffixes = ["ESQ", "PHD", "MD"]
+  @suffixes ["ESQ", "PHD", "MD"]
 
-  @ln_prefixes = [
-    "DE",
-    "DA",
-    "DI",
-    "LA",
-    "DU",
-    "DEL",
-    "DEI",
-    "VDA",
-    "DELLO",
-    "DELLA",
-    "DEGLI",
-    "DELLE",
-    "VAN",
-    "VON",
-    "DER",
-    "DEN",
-    "HEER",
-    "TEN",
-    "TER",
-    "VANDE",
-    "VANDEN",
-    "VANDER",
-    "VOOR",
-    "VER",
-    "AAN",
-    "MC",
-    "BEN",
-    "SAN",
-    "SAINZ",
-    "BIN",
-    "LI",
-    "LE",
-    "DES",
-    "AM",
-    "AUS'M",
-    "VOM",
-    "ZUM",
-    "ZUR",
-    "TEN",
-    "IBN"
+  @ln_prefixes [
+    "DE", "DA", "DI", "LA", "DU", "DEL", "DEI", "VDA", "DELLO", "DELLA",
+    "DEGLI", "DELLE", "VAN", "VON", "DER", "DEN", "HEER", "TEN", "TER",
+    "VANDE", "VANDEN", "VANDER", "VOOR", "VER", "AAN", "MC", "BEN", "SAN",
+    "SAINZ", "BIN", "LI", "LE", "DES", "AM", "AUS\"M", "VOM", "ZUM", "ZUR",
+    "TEN", "IBN"
   ]
 
-  @no_name = ["AKA", "FKA", "NKA", "FICTITIOUS"]
+  @non_name ["AKA", "FKA", "NKA", "FICTITIOUS"]
 
-  @corp_entity = [
-    "NA",
-    "CORP",
-    "CO",
-    "INC",
-    "ASSOCIATES",
-    "SERVICE",
-    "LLC",
-    "LLP",
-    "PARTNERS",
-    "RA",
-    "CO",
-    "COUNTY",
-    "STATE",
-    "BANK",
-    "GROUP",
-    "MUTUAL",
-    "FARGO"
+  @corp_entity [
+    "NA", "CORP", "CO", "INC", "ASSOCIATES", "SERVICE", "LLC", "LLP", "PARTNERS",
+    "RA", "CO", "COUNTY", "STATE", "BANK", "GROUP", "MUTUAL", "FARGO"
   ]
 
-  @supplemental_info = ["WIFE OF", "HUSBAND OF", "SON OF", "DAUGHTER OF", "DECEASED"]
+  @supplemental_info ["WIFE OF", "HUSBAND OF", "SON OF", "DAUGHTER OF", "DECEASED"]
 
   @doc """
-  Hello world.
+  Parse a full name string into its individual parts.
 
   ## Examples
 
-      iex> Nameparts.hello
-      :world
+      iex> {:ok, parts} = Nameparts.parse("Bruce Wayne a/k/a Batman")
+      iex> parts.first_name
+      "Bruce"
+      iex> parts.last_name
+      "Wayne"
+      iex> parts.aliases |> Enum.at(0)
+      "Batman"
+      iex> parts.salutation
+      nil
+      iex> parts.has_non_name
+      true
+      iex> parts.has_corporate_entity
+      false
 
   """
-  def hello do
-    :world
+  def parse(name) when is_binary(name) do
+    name_pieces =
+      modify_name(name)
+      |> String.split(" ")
+      |> Enum.with_index()
+
+    output =
+      {%Nameparts{full_name: name}, name_pieces}
+      |> determine_parts(name_pieces)
+
+    {:ok, output}
+  end
+
+  def parse(_), do: {:error, "must be string"}
+
+  # remove unwanted chars and consecutive spaces
+  defp modify_name(name) do
+    name = Regex.replace(~r/[.,\/\\]/, name, "")
+    Regex.replace(~r/\s\s+/, name, " ")
+  end
+
+  defp determine_parts({%Nameparts{} = output, name_pieces}, []) do
+    name_pieces =
+      Enum.filter(name_pieces, fn {str, _} -> String.trim(str) != "" end)
+      |> Enum.map(fn {str, _} -> str end)
+
+    first_name = Enum.at(name_pieces, 0)
+    {middle_name, last_name} = Enum.drop(name_pieces, 1) |> get_middle_last()
+
+    %{output | first_name: first_name, middle_name: middle_name, last_name: last_name}
+  end
+
+  defp determine_parts({%Nameparts{} = output, name_pieces}, [head | tail]) do
+    case determine_part(output, head) do
+      {output, {:remove, index}} ->
+        new_pieces = clear_by_index(name_pieces, index)
+        determine_parts({output, new_pieces}, tail)
+
+      {output, {:concat_next, index}} ->
+        [_ | new_tail] = tail
+        new_pieces = concat_next(name_pieces, index, 1)
+        determine_parts({output, new_pieces}, new_tail)
+
+      {output, {:handle_alias, quote_type, index}} ->
+        {new_output, new_pieces, new_tail} =
+          handle_alias({output, name_pieces}, index, {quote_type, tail})
+
+        determine_parts({new_output, new_pieces}, new_tail)
+
+      {output, :keep} ->
+        determine_parts({output, name_pieces}, tail)
+    end
+  end
+
+  defp get_middle_last(name_pieces) do
+    do_get_middle_last(name_pieces, [])
+  end
+
+  defp do_get_middle_last([], []), do: {nil, nil}
+
+  defp do_get_middle_last([head | []], []), do: {nil, head}
+
+  defp do_get_middle_last([head | []], middle) do
+    {Enum.join(middle, " "), head}
+  end
+
+  defp do_get_middle_last([head | tail], middle) do
+    do_get_middle_last(tail, middle ++ [head])
+  end
+
+  defp determine_part(%Nameparts{} = output, {name_piece, index}) do
+    cond do
+      # ignore "the"
+      String.downcase(name_piece) == "the" ->
+        {output, {:remove, index}}
+
+      # salutation?
+      is_nil(output.salutation) and is_in_list?(@salutations, name_piece) ->
+        {%{output | salutation: name_piece}, {:remove, index}}
+
+      # generation?
+      is_nil(output.generation) and is_in_list?(@generations, name_piece) ->
+        {%{output | generation: name_piece}, {:remove, index}}
+
+      # suffix?
+      is_nil(output.suffix) and is_in_list?(@suffixes, name_piece) ->
+        {%{output | suffix: name_piece}, {:remove, index}}
+
+      # has LN prefix?
+      not output.has_ln_prefix and is_in_list?(@ln_prefixes, name_piece) and index != 0 ->
+        {%{output | has_ln_prefix: true}, {:concat_next, index}}
+
+      # alias?
+      is_in_list?(@non_name, name_piece) ->
+        {%{output | has_non_name: true}, {:handle_alias, :non_quote, index}}
+
+      # quoted alias?
+      String.first(name_piece) == "'" ->
+        {%{output | has_non_name: true}, {:handle_alias, :single_quote, index}}
+
+      String.first(name_piece) == "\"" ->
+        {%{output | has_non_name: true}, {:handle_alias, :double_quote, index}}
+
+      # has corporate entity?
+      not output.has_corporate_entity and is_in_list?(@corp_entity, name_piece) ->
+        {%{output | has_corporate_entity: true}, :keep}
+
+      # has supplemental info?
+      not output.has_supplemental_info and is_in_list?(@supplemental_info, name_piece) ->
+        {%{output | has_supplemental_info: true}, {:remove, index}}
+
+      # everything else
+        true -> {output, :keep}
+    end
+  end
+
+  defp is_in_list?(list_attr, name_piece) do
+    upper = String.upcase(name_piece)
+    Enum.member?(list_attr, upper)
+  end
+
+  defp clear_by_index(name_pieces, index) do
+    name_pieces
+    |> Enum.map(fn {_, v} = existing ->
+      cond do
+        v == index -> {"", v}
+        true -> existing
+      end
+    end)
+  end
+
+  defp concat_next(name_pieces, index, concat_count) do
+    last_id = index + concat_count
+
+    new_str =
+      Enum.slice(name_pieces, index..last_id)
+      |> Enum.map(fn {k, _} -> k end)
+      |> Enum.join(" ")
+
+    name_pieces
+    |> Enum.map(fn {_, v} = existing ->
+      cond do
+        v < index -> existing
+        v == index -> {new_str, v}
+        v <= last_id -> {"", v}
+        true -> existing
+      end
+    end)
+  end
+
+  defp handle_alias({output, name_pieces}, index, {quote_type, remaining_pieces}) do
+    count = count_alias({quote_type, remaining_pieces})
+    new_tail = Enum.drop(remaining_pieces, count)
+    {new_output, new_pieces} = get_alias_output(output, name_pieces, {index, count, quote_type})
+
+    {new_output, new_pieces, new_tail}
+  end
+
+  defp count_alias({:non_quote, [{str, _} | _]}) do
+    case String.downcase(str) do
+      "the" -> 2
+      _ -> 0
+    end
+  end
+
+  defp count_alias({:double_quote, remaining_pieces}) do
+    do_count_alias("\"", remaining_pieces, 0)
+  end
+
+  defp count_alias({:single_quote, remaining_pieces}) do
+    do_count_alias("'", remaining_pieces, 0)
+  end
+
+  defp do_count_alias(_, [], _), do: 0
+
+  defp do_count_alias(quo, [{str, _} | tail], num) do
+    case String.ends_with?(str, quo) do
+      false -> do_count_alias(quo, tail, num + 1)
+      true -> num + 1
+    end
+  end
+
+  defp get_alias_output(output, name_pieces, {index, count, :non_quote}) do
+    name_pieces =
+      case count do
+        0 ->
+          clear_by_index(name_pieces, index)
+
+        2 ->
+          clear_by_index(name_pieces, index)
+          |> concat_next(index + 1, 1)
+      end
+
+    {a, _} = Enum.at(name_pieces, index + 1)
+    new_pieces = clear_by_index(name_pieces, index + 1)
+    new_aliases = add_alias(output.aliases, a)
+
+    {%{output | aliases: new_aliases}, new_pieces}
+  end
+
+  defp get_alias_output(output, name_pieces, {index, count, _}) do
+    name_pieces = concat_next(name_pieces, index, count)
+    {a, _} = Enum.at(name_pieces, index)
+    a = a |> String.trim("'") |> String.trim("\"")
+    new_pieces = clear_by_index(name_pieces, index)
+    new_aliases = add_alias(output.aliases, a)
+
+    {%{output | aliases: new_aliases}, new_pieces}
+  end
+
+  defp add_alias(aliases, a) do
+    case is_nil(aliases) do
+      true -> [a]
+      false -> aliases ++ [a]
+    end
   end
 end
--- test/nameparts_test.exs
+++ test/nameparts_test.exs
@@ -3,12 +3,12 @@ defmodule NamepartsTest do
   doctest Nameparts
 
   test "returns a Nameparts struct" do
-    name_parts = Nameparts.parse("John")
+    {:ok, name_parts} = Nameparts.parse("John")
     assert name_parts == %Nameparts{full_name: "John", first_name: "John"}
   end
 
   test "parses a simple name" do
-    name_parts = Nameparts.parse("John Jacob")
+    {:ok, name_parts} = Nameparts.parse("John Jacob")
 
     # Parse results
     assert name_parts.full_name == "John Jacob"
@@ -30,7 +30,7 @@ defmodule NamepartsTest do
   end
 
   test "parses a simple name, whose first name matches a LN Prefix" do
-    name_parts = Nameparts.parse("Ben Franklin")
+    {:ok, name_parts} = Nameparts.parse("Ben Franklin")
 
     # Parse results
     assert name_parts.full_name == "Ben Franklin"
@@ -52,7 +52,7 @@ defmodule NamepartsTest do
   end
 
   test "parses a simple name with a single middle name" do
-    name_parts = Nameparts.parse("Neil Patrick Harris")
+    {:ok, name_parts} = Nameparts.parse("Neil Patrick Harris")
 
     # Parse results
     assert name_parts.full_name == "Neil Patrick Harris"
@@ -74,7 +74,7 @@ defmodule NamepartsTest do
   end
 
   test "parses a spaced surname" do
-    name_parts = Nameparts.parse("Otto Von Bismark")
+    {:ok, name_parts} = Nameparts.parse("Otto Von Bismark")
 
     # Parse results
     assert name_parts.full_name == "Otto Von Bismark"
@@ -96,7 +96,7 @@ defmodule NamepartsTest do
   end
 
   test "parses an apostrophe surname" do
-    name_parts = Nameparts.parse("Scarlett O'Hara")
+    {:ok, name_parts} = Nameparts.parse("Scarlett O'Hara")
 
     # Parse results
     assert name_parts.full_name == "Scarlett O'Hara"
@@ -118,7 +118,7 @@ defmodule NamepartsTest do
   end
 
   test "parses a generation name" do
-    name_parts = Nameparts.parse("Thurston Howell III")
+    {:ok, name_parts} = Nameparts.parse("Thurston Howell III")
 
     # Parse results
     assert name_parts.full_name == "Thurston Howell III"
@@ -140,7 +140,7 @@ defmodule NamepartsTest do
   end
 
   test "parses a generation name designated by the word 'the'" do
-    name_parts = Nameparts.parse("Thurston Howell the 3rd")
+    {:ok, name_parts} = Nameparts.parse("Thurston Howell the 3rd")
 
     # Parse results
     assert name_parts.full_name == "Thurston Howell the 3rd"
@@ -162,7 +162,7 @@ defmodule NamepartsTest do
   end
 
   test "parses a generation name designated by the spelled out word" do
-    name_parts = Nameparts.parse("Thurston Howell Third")
+    {:ok, name_parts} = Nameparts.parse("Thurston Howell Third")
 
     # Parse results
     assert name_parts.full_name == "Thurston Howell Third"
@@ -184,7 +184,7 @@ defmodule NamepartsTest do
   end
 
   test "parses a generation name designated by the word 'the' and the spelled out generation" do
-    name_parts = Nameparts.parse("Thurston Howell the Third")
+    {:ok, name_parts} = Nameparts.parse("Thurston Howell the Third")
 
     # Parse results
     assert name_parts.full_name == "Thurston Howell the Third"
@@ -206,14 +206,14 @@ defmodule NamepartsTest do
   end
 
   test "parses a single alias name" do
-    name_parts = Nameparts.parse("Bruce Wayne a/k/a Batman")
+    {:ok, name_parts} = Nameparts.parse("Bruce Wayne a/k/a Batman")
 
     # Parse results
     assert name_parts.full_name == "Bruce Wayne a/k/a Batman"
     assert name_parts.first_name == "Bruce"
     assert name_parts.last_name == "Wayne"
     assert name_parts.has_non_name == true
-    assert name_parts.aliases |> Enum.fetch(0) == {:ok, "Batman"}
+    assert name_parts.aliases |> Enum.at(0) == "Batman"
 
     # Members not used for this result
     assert is_nil(name_parts.salutation)
@@ -227,15 +227,15 @@ defmodule NamepartsTest do
     assert name_parts.has_supplemental_info == false
   end
 
-  test "parses a nick name with one word" do
-    name_parts = Nameparts.parse("\"Stonecold\" Steve Austin")
+  test "parses a nickname with one word" do
+    {:ok, name_parts} = Nameparts.parse("\"Stonecold\" Steve Austin")
 
     # Parse results
     assert name_parts.full_name == "\"Stonecold\" Steve Austin"
     assert name_parts.first_name == "Steve"
     assert name_parts.last_name == "Austin"
     assert name_parts.has_non_name == true
-    assert name_parts.aliases |> Enum.fetch(0) == {:ok, "Stonecold"}
+    assert name_parts.aliases |> Enum.at(0) == "Stonecold"
 
     # Members not used for this result
     assert is_nil(name_parts.salutation)
@@ -249,15 +249,15 @@ defmodule NamepartsTest do
     assert name_parts.has_supplemental_info == false
   end
 
-  test "parses a nick name with two word" do
-    name_parts = Nameparts.parse("Dwayne \"The Rock\" Johnson")
+  test "parses a nickname with two words" do
+    {:ok, name_parts} = Nameparts.parse("Dwayne \"The Rock\" Johnson")
 
     # Parse results
     assert name_parts.full_name == "Dwayne \"The Rock\" Johnson"
     assert name_parts.first_name == "Dwayne"
     assert name_parts.last_name == "Johnson"
     assert name_parts.has_non_name == true
-    assert name_parts.aliases |> Enum.fetch(0) == {:ok, "The Rock"}
+    assert name_parts.aliases |> Enum.at(0) == "The Rock"
 
     # Members not used for this result
     assert is_nil(name_parts.salutation)
@@ -272,14 +272,14 @@ defmodule NamepartsTest do
   end
 
   test "parses a nick name with many spaces" do
-    name_parts = Nameparts.parse("\"The Nature Boy\" Ric Flair")
+    {:ok, name_parts} = Nameparts.parse("\"The Nature Boy\" Ric Flair")
 
     # Parse results
     assert name_parts.full_name == "\"The Nature Boy\" Ric Flair"
     assert name_parts.first_name == "Ric"
     assert name_parts.last_name == "Flair"
     assert name_parts.has_non_name == true
-    assert name_parts.aliases |> Enum.fetch(0) == {:ok, "The Nature Boy"}
+    assert name_parts.aliases |> Enum.at(0) == "The Nature Boy"
 
     # Members not used for this result
     assert is_nil(name_parts.salutation)
@@ -294,21 +294,21 @@ defmodule NamepartsTest do
   end
 
   test "parses multiple aliases" do
-    name_parts =
+    {:ok, name_parts} =
       Nameparts.parse(
         "\"The People's Champion\" Mohammed \"Louisville Lip\" Ali aka The Greatest"
       )
 
     # Parse results
     assert name_parts.full_name ==
-      "\"The People's Champion\" Mohammed \"Louisville Lip\" Ali aka The Greatest"
+             "\"The People's Champion\" Mohammed \"Louisville Lip\" Ali aka The Greatest"
 
     assert name_parts.first_name == "Mohammed"
     assert name_parts.last_name == "Ali"
     assert name_parts.has_non_name == true
-    assert name_parts.aliases |> Enum.fetch(0) == {:ok, "The People's Champion"}
-    assert name_parts.aliases |> Enum.fetch(1) == {:ok, "Louisville Lip"}
-    assert name_parts.aliases |> Enum.fetch(2) == {:ok, "The Greatest"}
+    assert name_parts.aliases |> Enum.at(0) == "The People's Champion"
+    assert name_parts.aliases |> Enum.at(1) == "Louisville Lip"
+    assert name_parts.aliases |> Enum.at(2) == "The Greatest"
 
     # Members not used for this result
     assert is_nil(name_parts.salutation)
@@ -323,7 +323,7 @@ defmodule NamepartsTest do
   end
 
   test "parses supplemental information" do
-    name_parts = Nameparts.parse("Philip Francis \"The Scooter\" Rizzuto, deceased")
+    {:ok, name_parts} = Nameparts.parse("Philip Francis \"The Scooter\" Rizzuto, deceased")
 
     # Parse results
     assert name_parts.full_name == "Philip Francis \"The Scooter\" Rizzuto, deceased"
@@ -331,7 +331,7 @@ defmodule NamepartsTest do
     assert name_parts.middle_name == "Francis"
     assert name_parts.last_name == "Rizzuto"
     assert name_parts.has_non_name == true
-    assert name_parts.aliases |> Enum.fetch(0) == {:ok, "The Scooter"}
+    assert name_parts.aliases |> Enum.at(0) == "The Scooter"
     assert name_parts.has_supplemental_info == true
 
     # Members not used for this result
@@ -345,7 +345,7 @@ defmodule NamepartsTest do
   end
 
   test "parses a name with multiple middle names" do
-    name_parts = Nameparts.parse("George Herbert Walker Bush")
+    {:ok, name_parts} = Nameparts.parse("George Herbert Walker Bush")
     assert name_parts.first_name == "George"
     assert name_parts.middle_name == "Herbert Walker"
     assert name_parts.last_name == "Bush"
